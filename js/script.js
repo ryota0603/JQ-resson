4f7129582c96e3ba7a8fd813c404b0bf54d2c923
ryota0603 <114867300+ryota0603@users.noreply.github.com>
2023-02-06 05:57:03 +0000

    seccond

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
+// $('.セレクタ名').on('click', function(event) {
+//   イベント発生時に行われる処理
+// });
+
 $(function() {
   $('#back a').on('click',function(event){
-     $('body, html').animate({
+    $('body, html').animate({
       scrollTop:0
     }, 800);
   event.preventDefault();
   });
 });
+
